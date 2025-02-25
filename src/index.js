0ce7fc0aa765416867745246a787713ea0b7aed2
AdrianNetto <adrian.netto73@gmail.com>
2025-02-25 11:37:38 -0300

    feat: many things

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import mongoose, { mongo } from "mongoose";
+import app from "./app.js";
+import logger from "./configs/logger.config.js";
+
+const PORT = process.env.PORT || 8000;
+const { DATABASE_URL } = process.env;
+
+mongoose.createConnection("error", (err) => {
+  logger.error("MongoDB connection error: ", err);
+  process.exit(1);
+});
+
+if(process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  });
+
+let server = app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+
+  logger.info("Server is shutting down");
+  process.exit();
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
+});
